c1f6ca06aec2c1943552e466fd45334688cc45ac
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-05-12 13:58:50 +0300

    Initial Commit

---
+++ RedYellowGreenSwiftUI/RedYellowGreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedYellowGreenSwiftUIApp.swift
+//  RedYellowGreenSwiftUI
+//
+//  Created by Александра Лесовская on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RedYellowGreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
